c165eda1d5eb58df5a717c90c8a6c7da4b1c7173
PonyCui <cuiminghui@yy.com>
2015-03-25 14:36:49 +0800

    javascript ws demo done

--- application/controllers/pubsub.php
+++ application/controllers/pubsub.php
@@ -11,11 +11,15 @@ class PubSub extends CI_Controller implements MessageComponentInterface
 
     protected $clients;
 
+    protected $services;
+
     public function __construct() {
         parent::__construct();
         $this->load->helpers('Pms_protocol');
-        $this->load->model('Sub_manager');
         $this->load->model('Token_entity');
+        $this->services = array(
+            'sub' => new SubService
+        );
         $this->clients = new \SplObjectStorage;
     }
 
@@ -27,27 +31,63 @@ class PubSub extends CI_Controller implements MessageComponentInterface
         $service = pms_service($msg);
         $method = pms_method($msg);
         $params = pms_params($msg);
-        if ($service == 'sub') {
-            if ($method == 'addObserver') {
-                $tokenObject = new Token_entity;
-                $tokenObject -> user_id = isset($params['user_id']) ? $params['user_id'] : '';
-                $tokenObject -> session_token = isset($params['session_token']) ? $params['session_token'] : '';
-                $this->Sub_manager->addObserver($from, $tokenObject);
+        if (isset($this->services[$service])) {
+            if (method_exists($this->services[$service], $method)) {
+                $this->services[$service] -> $method($from, $params);
             }
         }
-
-        // foreach ($this->clients as $client) {
-        //     if ($from != $client) {
-        //         $client->send($msg);
-        //     }
-        // }
     }
 
     public function onClose(ConnectionInterface $conn) {
+        $this->services['sub']->removeObserver($conn);
         $this->clients->detach($conn);
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
         $conn->close();
+    }
+}
+
+/**
+ *
+ */
+class SubService extends CI_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Sub_manager');
+    }
+
+    public function addObserver($conn, $params)
+    {
+        $tokenObject = new Token_entity;
+        $tokenObject -> user_id = isset($params['user_id']) ? $params['user_id'] : '';
+        $tokenObject -> session_token = isset($params['session_token']) ? $params['session_token'] : '';
+        $result = $this->Sub_manager->addObserver($conn, $tokenObject);
+        if ($result) {
+            $this -> didAddObserver($conn);
+        }
+    }
+
+    public function removeObserver($conn)
+    {
+        $this->Sub_manager->removeObserver($conn);
+    }
+
+    public function didAddObserver($conn)
+    {
+
+    }
+
+    public function didRemoveObserver($conn)
+    {
+
+    }
+
+    public function didReceivedError($conn, $error_description)
+    {
+
     }
 }
