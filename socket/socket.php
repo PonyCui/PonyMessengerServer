54de7fff5ceec7d00212da3725e4a9123584cc4e
PonyCui <cuiminghui@yy.com>
2015-03-26 15:54:20 +0800

    socket类型连接迁移至socket文件夹

--- socket/socket.php
+++ socket/socket.php
@@ -7,8 +7,8 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 require __DIR__ . '/Ratchet/vendor/autoload.php';
-require __DIR__ . '/index.php';
-require __DIR__ . '/application/controllers/pubsub.php';
+require __DIR__ . '/../index.php';
+require __DIR__ . '/../application/controllers/pubsub.php';
 
 $app = new Ratchet\App('localhost', 9090);
 $app->route('/pubsub', new PubSub, array('*'));
