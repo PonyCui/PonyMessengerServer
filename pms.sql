dae8039d26ef689cf8cb7d12cbe2686a1a86c8ad
PonyCui <cuiminghui@yy.com>
2015-03-27 17:26:39 +0800

    update sql

--- pms.sql
+++ pms.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: 127.0.0.1
--- 生成日期: 2015 年 03 月 26 日 10:28
+-- 生成日期: 2015 年 03 月 27 日 09:26
 -- 服务器版本: 5.6.14
 -- PHP 版本: 5.5.14
 
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `pms_pub` (
   `sub_params` text NOT NULL,
   PRIMARY KEY (`pub_id`),
   KEY `sub_user_id` (`sub_user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12041 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12048 ;
 
 -- --------------------------------------------------------
 
@@ -49,4 +49,26 @@ CREATE TABLE IF NOT EXISTS `pms_token` (
 
 INSERT INTO `pms_token` (`user_id`, `session_token`, `session_access`) VALUES
 (1, 'testToken', ''),
-(2, 'testToken', 'pub');
+(2, 'testToken', 'pub'),
+(9, 'a5116a8d10b7b0419b00d313efa2aebc', '');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `pms_user_base`
+--
+
+CREATE TABLE IF NOT EXISTS `pms_user_base` (
+  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+
+--
+-- 转存表中的数据 `pms_user_base`
+--
+
+INSERT INTO `pms_user_base` (`user_id`, `email`, `password`) VALUES
+(9, 'ponycui@me.com', 'c4f34afe14817af7fbb175da2e609dd3');
