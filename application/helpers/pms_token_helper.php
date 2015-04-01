8d436833711208cfc57759db8926209d1e0d64d8
PonyCui <cuiminghui@yy.com>
2015-04-01 17:12:04 +0800

    change env

--- application/helpers/pms_token_helper.php
+++ application/helpers/pms_token_helper.php
@@ -6,8 +6,8 @@ function pms_verify_token(CI_Controller $controller, &$tokenRef = null)
     $token = new Token_entity;
     $token->user_id = $controller->input->cookie('user_id');
     $token->session_token = $controller->input->cookie('session_token');
-    $token->user_id = 1;
-    $token->session_token = 'testToken';
+    // $token->user_id = 1;
+    // $token->session_token = 'testToken';
     $tokenRef = $token;
     return $controller->Token_manager->verify_entry($token);
 }
